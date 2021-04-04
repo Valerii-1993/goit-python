cfb5537ccc96bef24b21e102e97466da288dfdb9
Valerii-1993 <75755494+Valerii-1993@users.noreply.github.com>
2021-04-04 13:28:54 +0300

    hw11

--- lesson11/hw11.py
+++ lesson11/hw11.py
@@ -1,76 +1,37 @@
+import re
 from collections import UserDict
 from datetime import datetime 
-from pickle import dump, load
-import re
 
 
 class AddressBook(UserDict):
 
-    def __init__(self):
-        self.data = {}
-        self.counter = -1
-
-    def __str__(self): 
-        result = []
-
-        for key, value in self.data.items():
-            birth = value[1].strftime("%d/%m/%Y")
-            if value[0]:
-                new_value = []
-                for number in value[0]:
-                    if number:
-                        new_value.append(number)
-                phone = ', '.join(new_value)
-            result.append(f"Name: {key} - phones: {phone} - birthday: {birth}")
-        return '\n'.join(result)
-
-    def __next__(self):
-        keys = list(self.data.keys())
-        self.counter += 1
-
-        if self.counter == len(keys):
-            self.counter = -1
-            raise StopIteration
-        result = f"{keys[self.counter]}:{self.data[keys[self.counter]]}"
-        return result
-
-    def __iter__(self):
-        return self
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-
-    def __getitem__(self, key):
-        return self.data[key]
+    data = {}
 
     def add(self, record):
         account = {record.name:[record.phones, record.birthday]}
         self.data.update(account)
 
-    def save(self, file_name):
-        with open(file_name, 'wb') as file:
-            dump(self.data, file_name)
+    def __str__(self): 
+        result = []
 
-    def load(self, file_name):
-        with open(file_name, 'rb') as file:
-            unpacked_data = load(file_name)
-        return unpacked_data
+        for key, value in self.data.items():
+            birth = value[0].strftime(" %d/%m/%Y ")
+            phone = ', '.join(value[1])
+            result.append(f"Name: {key} - phones: {phone}, - birthday: {birth}")
+        return '\n'.join(result)
 
-    def search(self, pattern):
-        keys = list(self.data.keys())
-        values = list(self.data.values())
-        result = ""
-        for key in keys:
-            if key.lower().startswith(pattern.lower()):
-                result += f"{keys[keys.index(key)]}:{self.data[keys[keys.index(key)]]}\n"
-        for value in values:
-            for number in value[0]:
-                if number.lower().startswith(pattern.lower()):
-                    result += f"{number.lower()}\n"
-        return result
-       
-            
+    def __next__(self):
+        keys = []
+        counter = 0
+        for key in self.data.keys():
+            keys.append(key)
+        if counter < len(keys):
+            yield keys[counter], self.data(keys[counter])
+            counter += 1
+        raise StopIteration
 
+    def __iter__(self):
+        return AddressBook()
 
 class Record:
 
@@ -80,7 +41,7 @@ class Record:
         self.name = Name(name).value
         self.phones = []
         for phone in phones:
-            self.phones.append(Phone(phone).value)
+            self.phones += Phone(phone).value
         
     def days_to_birthday (self):
         current_datetime = datetime.now()
@@ -92,7 +53,6 @@ class Record:
             result = self.birthday - current_datetime
         return result.days
 
-
 class Field:
 
     def __init__(self, value):
@@ -101,22 +61,11 @@ class Field:
     def __getitem__(self):
         return self.value
 
-
 class Name(Field):
     pass
 
 
 class Phone(Field):
-    
-    def __init__(self, value):
-        try:
-            if re.match('^\+48\d{9}$', value):
-                self.value = value
-            else: 
-                raise ValueError
-        except ValueError:
-            self.value = ""
-            print('Incorrect phone number!')
 
     def __setitem__(self, new_value):
         try:
@@ -125,7 +74,7 @@ class Phone(Field):
             else: 
                 raise ValueError
         except ValueError:
-            print('Incorrect phone number!')
+            print('Incorrect phone number! Please provide correct phone number.')
 
 
 class Birthday(Field):
@@ -140,4 +89,4 @@ class Birthday(Field):
             else:
                 raise ValueError
         except ValueError:
-            print('Incorrect date!')
+            print('Incorrect date! Please provide correct date format.')
